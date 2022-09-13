6cd5fbdf938b236be158f2dbfab00806e48b5be4
Terence Waters <12157591+AplUSAndmINUS@users.noreply.github.com>
2022-09-13 15:51:10 -0600

    removed and added correct bootstrap dependencies

--- src/component/card/card.tsx
+++ src/component/card/card.tsx
@@ -1,9 +1,16 @@
 import React from 'react';
+import { Card } from 'react-bootstrap';
 
 import './styles.scss';
 
-export const Card = () => {
+interface CardProps {
+  color?: string;
+  suit: JSX.Element;
+  value: string;
+}
+
+export const PokerCard = ({ color, suit, value }: CardProps) => {
   return (<h1>I am a card!</h1>)
 };
 
-export default Card;
+export default PokerCard;
