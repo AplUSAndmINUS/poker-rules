53a9e20bf9da1d1e26ea012dea12a5c372d4dafa
Terence Waters <12157591+AplUSAndmINUS@users.noreply.github.com>
2022-09-13 15:06:15 -0600

    updated project's structure for prep in routing

--- src/index.tsx
+++ src/index.tsx
@@ -3,15 +3,14 @@ import ReactDOM from 'react-dom/client';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 import './styles/main.scss';
-import App from './App';
+
+const SampleApp = () => {
+  return ( <h1>Hi React!</h1> )
+}
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <SampleApp />
   </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
