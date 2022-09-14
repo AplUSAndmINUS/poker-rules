307b597fc5efd3a1d01e4ab7587e7ada02797929
Terence Waters <12157591+AplUSAndmINUS@users.noreply.github.com>
2022-09-13 19:56:19 -0600

    finished styling of 404

--- src/pages/404/404.tsx
+++ src/pages/404/404.tsx
@@ -1,9 +1,12 @@
 import React from 'react';
-
-import './styles.scss';
-
+  
 export const NotFound = () => {
-  return (<h1>I am the 404 Error page!</h1>)
+  return (
+    <div className="d-flex flex-column justify-content-center align-items-center">
+      <h1 className="text-center mt-5 py-2">Whoops! <br />404- Page Not Found</h1>
+      <p className="text-center font-weight-bold px-4">We can't find the page you're looking for. <br />Please select one of the menu options above to go back.</p>
+    </div>
+  )
 }
 
 export default NotFound;
