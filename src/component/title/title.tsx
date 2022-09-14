c291d2a5ebca7ce006629654c59f12d347429dc6
Terence Waters <12157591+AplUSAndmINUS@users.noreply.github.com>
2022-09-14 08:05:39 -0600

    fixed title text transform

--- src/component/title/title.tsx
+++ src/component/title/title.tsx
@@ -11,7 +11,9 @@ interface TitleProps {
 
 export const Title = ({ heading = "Title" }: TitleProps) => {
   return (
-    <div className="mt-4 d-flex justify-content-center align-items-baseline">
+    <div
+      className="mt-4 d-flex justify-content-center align-items-baseline text-titlecase"
+      style={{ textTransform: 'titlecase' }}>
       <Club size={24} />
       <Diamond size={24} />
       <h1 className="mx-2">{heading}</h1>
