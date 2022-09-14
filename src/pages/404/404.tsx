2ff8fe8950182598b399e82d29a1c9cb49f2666f
Terence Waters <12157591+AplUSAndmINUS@users.noreply.github.com>
2022-09-14 06:03:35 -0600

    layout fixes to remove title from layout

--- src/pages/404/404.tsx
+++ src/pages/404/404.tsx
@@ -5,7 +5,7 @@ import Title from '../../component/title/title';
 export const NotFound = () => {
   return (
     <div className="d-flex flex-column justify-content-center align-items-center">
-      <Title heading="Whoops! 404- Page Not Found" />
+      <Title heading="404 Page Not Found" />
       <p className="text-center font-weight-bold px-4">We can't find the page you're looking for. <br />Please select one of the menu options above to go back.</p>
     </div>
   )
