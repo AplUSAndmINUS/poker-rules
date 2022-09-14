a89210b1cabe43564890cd917518e49ac9c5d4f6
Terence Waters <12157591+AplUSAndmINUS@users.noreply.github.com>
2022-09-14 06:09:07 -0600

    initial set up for ranking card

--- src/pages/hand-rankings/hand-rankings.tsx
+++ src/pages/hand-rankings/hand-rankings.tsx
@@ -1,9 +1,17 @@
 import React from 'react';
 
+import Title from '../../component/title/title';
 import './styles.scss';
+import './styled';
+import RankingCard from './ranking-card/ranking-card';
 
 export const Rankings = () => {
-  return (<h1>I am the hand rankings!</h1>)
+  return (
+    <div className="d-flex flex-column align-items-center">
+      <Title heading="Hand rankings"/>
+      <RankingCard />
+    </div>
+  )
 }
 
 export default Rankings;
---
+++ src/pages/hand-rankings/ranking-card/ranking-card.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+import './styled';
+
+export const RankingCard = () => {
+  return <h1>Ranking Card!</h1>
+};
+
+export default RankingCard
