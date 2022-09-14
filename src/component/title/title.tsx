a523826600ed775f2d2451d48697af65371e2fa2
Terence Waters <12157591+AplUSAndmINUS@users.noreply.github.com>
2022-09-14 08:03:48 -0600

    fixed title spacing

--- src/component/title/title.tsx
+++ src/component/title/title.tsx
@@ -11,7 +11,7 @@ interface TitleProps {
 
 export const Title = ({ heading = "Title" }: TitleProps) => {
   return (
-    <div className="mt-3 d-flex justify-content-center align-items-baseline">
+    <div className="mt-4 d-flex justify-content-center align-items-baseline">
       <Club size={24} />
       <Diamond size={24} />
       <h1 className="mx-2">{heading}</h1>
